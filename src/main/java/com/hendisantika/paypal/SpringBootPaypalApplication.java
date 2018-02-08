5e4f0b6b95c94409e335c2443719da60689a3b94
Hendi Santika <hendisantika@gmail.com>
2018-02-09 06:46:45 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/paypal/SpringBootPaypalApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.paypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPaypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPaypalApplication.class, args);
+	}
+}
