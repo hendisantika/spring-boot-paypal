c79413d7d792755c244cbbc4c2c36da9aadcbf06
Hendi Santika <hendisantika@gmail.com>
2021-07-06 12:45:05 +0700

    #61 - Add PaypalService

---
+++ src/main/java/com/hendisantika/paypal/service/PaypalService.java
@@ -0,0 +1,22 @@
+package com.hendisantika.paypal.service;
+
+import com.paypal.base.rest.APIContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-paypal
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/07/21
+ * Time: 12.44
+ */
+@Service
+public class PaypalService {
+
+    @Autowired
+    private APIContext apiContext;
+
+}
