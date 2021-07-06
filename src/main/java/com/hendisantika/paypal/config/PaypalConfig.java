fc47d76fbd01a39329ce4a50bc56bebc8b18f5c6
Hendi Santika <hendisantika@gmail.com>
2021-07-06 12:44:11 +0700

    #60 - Add apiContext in Paypal Config

--- src/main/java/com/hendisantika/paypal/config/PaypalConfig.java
+++ src/main/java/com/hendisantika/paypal/config/PaypalConfig.java
@@ -1,6 +1,8 @@
 package com.hendisantika.paypal.config;
 
+import com.paypal.base.rest.APIContext;
 import com.paypal.base.rest.OAuthTokenCredential;
+import com.paypal.base.rest.PayPalRESTException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,5 +39,12 @@ public class PaypalConfig {
     @Bean
     public OAuthTokenCredential oAuthTokenCredential() {
         return new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
+    }
+
+    @Bean
+    public APIContext apiContext() throws PayPalRESTException {
+        APIContext context = new APIContext(oAuthTokenCredential().getAccessToken());
+        context.setConfigurationMap(paypalSdkConfig());
+        return context;
     }
 }
