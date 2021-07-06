f6a9044bee0e6e12f14f5db8c8192d11f69ebb12
Hendi Santika <hendisantika@gmail.com>
2021-07-06 12:52:22 +0700

    #68 - Add successPay in Paypal2Controller

--- src/main/java/com/hendisantika/paypal/controller/Paypal2Controller.java
+++ src/main/java/com/hendisantika/paypal/controller/Paypal2Controller.java
@@ -1,6 +1,8 @@
 package com.hendisantika.paypal.controller;
 
+import com.hendisantika.paypal.model.Order;
 import com.hendisantika.paypal.service.PaypalService;
+import com.paypal.api.payments.Links;
 import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,7 +26,7 @@ public class Paypal2Controller {
     public static final String SUCCESS_URL = "pay/success";
     public static final String CANCEL_URL = "pay/cancel";
     @Autowired
-    PaypalService service;
+    PaypalService paypalService;
 
     @GetMapping("/")
     public String home() {
@@ -33,9 +36,9 @@ public class Paypal2Controller {
     @PostMapping("/pay")
     public String payment(@ModelAttribute("order") Order order) {
         try {
-            Payment payment = service.createPayment(order.getPrice(), order.getCurrency(), order.getMethod(),
-                    order.getIntent(), order.getDescription(), "http://localhost:9090/" + CANCEL_URL,
-                    "http://localhost:9090/" + SUCCESS_URL);
+            Payment payment = paypalService.createPayment(order.getPrice(), order.getCurrency(), order.getMethod(),
+                    order.getIntent(), order.getDescription(), "http://localhost:8080/" + CANCEL_URL,
+                    "http://localhost:8080/" + SUCCESS_URL);
             for (Links link : payment.getLinks()) {
                 if (link.getRel().equals("approval_url")) {
                     return "redirect:" + link.getHref();
@@ -52,5 +55,19 @@ public class Paypal2Controller {
     @GetMapping(value = CANCEL_URL)
     public String cancelPay() {
         return "cancel";
+    }
+
+    @GetMapping(value = SUCCESS_URL)
+    public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
+        try {
+            Payment payment = paypalService.executePayment(paymentId, payerId);
+            System.out.println(payment.toJSON());
+            if (payment.getState().equals("approved")) {
+                return "success";
+            }
+        } catch (PayPalRESTException e) {
+            System.out.println(e.getMessage());
+        }
+        return "redirect:/";
     }
 }
